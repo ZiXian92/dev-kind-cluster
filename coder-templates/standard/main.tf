5823b8a5e1963aa1785650e4a7d0164975deef6d
Zi Xian Qua <zixian1992@hotmail.com>
2026-02-21 12:16:31 +0800

    Working version of template with privileged docker.

--- coder-templates/standard/main.tf
+++ coder-templates/standard/main.tf
@@ -93,16 +93,30 @@ locals {
   preagent_script    = <<-EOT
     #!/bin/bash
     set -e
-    %{for extension in split(",", data.coder_parameter.vscode_extensions.value)}
-    /usr/local/bin/code-server --install-extension ${trimspace(extension)}
-    %{endfor}
 
-    cat <<'EOF' > /home/coder/.local/share/code-server/User/settings.json
+    # Start Docker daemon in the background
+    /usr/bin/dockerd &
+
+    # To-Do: Install other configured tools like Golang, kubectl, helm, etc. here
+
+    # Set up and run code-server
+    gosu coder /bin/bash <<'EOC'
+      set -e
+      # Install code-server extensions
+      %{for extension in split(",", data.coder_parameter.vscode_extensions.value)}
+      /usr/local/bin/code-server --install-extension ${trimspace(extension)}
+      %{endfor}
+
+      cat <<'EOF' > /home/coder/.local/share/code-server/User/settings.json
 ${data.coder_parameter.vscode_settings.value}
 EOF
 
-    # Start code-server in the background
-    /usr/local/bin/code-server --bind-addr 0.0.0.0:${local.coder_ws_port} --auth none &
+      # Start code-server in the background
+      /usr/local/bin/code-server --bind-addr 0.0.0.0:${local.coder_ws_port} --auth none &
+
+      # Start coder agent
+      ${coder_agent.main.init_script}
+    EOC
   EOT
 }
 
@@ -173,9 +187,51 @@ resource "coder_app" "code_server" {
   }
 }
 
+resource "kubernetes_persistent_volume_claim_v1" "workspace_home" {
+  metadata {
+    name      = "${lower(data.coder_workspace.me.name)}-home"
+    namespace = local.workspace_namepace
+  }
+
+  wait_until_bound = false
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "64Mi"
+      }
+      limits = {
+        storage = "2Gi"
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim_v1" "workspace_docker_data" {
+  metadata {
+    name      = "${lower(data.coder_workspace.me.name)}-docker-data"
+    namespace = local.workspace_namepace
+  }
+
+  wait_until_bound = false
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+      limits = {
+        storage = "5Gi"
+      }
+    }
+  }
+
+}
+
 # Kubernetes Deployment with code-server and dockerd containers
 resource "kubernetes_deployment_v1" "coder_workspace" {
-  count = data.coder_workspace.me.start_count > 0 ? 1 : 0
   metadata {
     name      = lower(data.coder_workspace.me.name)
     namespace = local.workspace_namepace
@@ -190,7 +246,7 @@ resource "kubernetes_deployment_v1" "coder_workspace" {
   }
 
   spec {
-    replicas = 1
+    replicas = data.coder_workspace.me.start_count > 0 ? 1 : 0
 
     strategy {
       type = "Recreate"
@@ -212,10 +268,23 @@ resource "kubernetes_deployment_v1" "coder_workspace" {
       }
 
       spec {
+        security_context {
+          seccomp_profile {
+            type = "RuntimeDefault"
+          }
+        }
+
         volume {
           name = "home"
-          empty_dir {
-            size_limit = "2Gi"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim_v1.workspace_home.metadata.0.name
+          }
+        }
+
+        volume {
+          name = "docker-data"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim_v1.workspace_docker_data.metadata.0.name
           }
         }
 
@@ -226,7 +295,7 @@ resource "kubernetes_deployment_v1" "coder_workspace" {
           image_pull_policy = "IfNotPresent"
           command           = ["/usr/bin/tini", "--", "/bin/bash", "-c"]
           args = [
-            "${local.preagent_script}\n${coder_agent.main.init_script}"
+            local.preagent_script,
           ]
 
           port {
@@ -248,6 +317,11 @@ resource "kubernetes_deployment_v1" "coder_workspace" {
           volume_mount {
             name       = "home"
             mount_path = "/home/coder"
+          }
+
+          volume_mount {
+            name       = "docker-data"
+            mount_path = "/var/lib/docker"
           }
 
           # Coder agent environment variables
@@ -287,13 +361,7 @@ resource "kubernetes_deployment_v1" "coder_workspace" {
           }
 
           security_context {
-            run_as_user                = 2001 # coder user from Dockerfile
-            allow_privilege_escalation = false
-            privileged                 = false
-            capabilities {
-              drop = ["ALL"]
-              add  = ["NET_BIND_SERVICE"]
-            }
+            privileged = true # Required for Docker
           }
         }
       }
